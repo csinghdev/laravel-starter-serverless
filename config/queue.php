5e5e459bfa09d30f83736c5f2db96511e82d83e3
Chandresh <chandresh989@gmail.com>
2020-08-06 23:58:37 +0530

    Using AWS SQS with bref

--- config/queue.php
+++ config/queue.php
@@ -57,6 +57,7 @@ return [
             'queue' => env('SQS_QUEUE', 'your-queue-name'),
             'suffix' => env('SQS_SUFFIX'),
             'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+            'token' => env('AWS_SESSION_TOKEN'),
         ],
 
         'redis' => [
